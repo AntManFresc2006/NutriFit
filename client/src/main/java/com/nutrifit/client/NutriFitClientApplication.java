68d7aef564e0d9385d901b522fed1a7346da5da2
Antonio Manuel <antoniomanuelfresco@gmail.com>
2026-03-14 02:48:25 +0100

    feat: crear base del cliente JavaFX para la gestión de alimentos

---
+++ client/src/main/java/com/nutrifit/client/NutriFitClientApplication.java
@@ -0,0 +1,28 @@
+package com.nutrifit.client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class NutriFitClientApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                NutriFitClientApplication.class.getResource("/com/nutrifit/client/food-view.fxml")
+        );
+
+        Scene scene = new Scene(fxmlLoader.load(), 1100, 650);
+
+        stage.setTitle("NutriFit - Gestión de alimentos");
+        stage.setScene(scene);
+        stage.setMinWidth(1000);
+        stage.setMinHeight(600);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
